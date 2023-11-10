c324ea62b96b963f63219de63b12c80d09348e74
Achmad Dinofaldi Firmansyah <aldi.novaldi.frmnsyah@gmail.com>
2023-11-10 09:50:43 +0700

    feat(bot): init webhook

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// telegram webhook telegram/webhook
+Route::post('telegram/webhook', [\App\Http\Controllers\Api\TelegramBotController::class, 'webhook']);
